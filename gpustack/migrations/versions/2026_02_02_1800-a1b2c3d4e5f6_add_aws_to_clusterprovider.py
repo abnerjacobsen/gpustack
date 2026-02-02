4fb746e97275e2d02391fa9f00e3e662eabb5dad
abner G Jacobsen <abner@apoana.com.br>
2026-02-02 18:11:23 -0300

    SECURITY: Remove docs/aws-debug-guide.md containing exposed AWS credentials
    - Reverted commit d5052cb4 which contained hardcoded AWS access keys
    - Credentials were exposed in documentation file (SECURITY ISSUE)
    - Users must revoke exposed credentials in AWS IAM Console immediately

--- gpustack/migrations/versions/2026_02_02_1800-a1b2c3d4e5f6_add_aws_to_clusterprovider.py
+++ gpustack/migrations/versions/2026_02_02_1800-a1b2c3d4e5f6_add_aws_to_clusterprovider.py
@@ -1,7 +1,7 @@
 """add aws to clusterprovider enum
 
-Revision ID: 2026_02_02_1800-a1b2c3d4e5f6_add_aws_to_clusterprovider
-Revises: 53667f33f000
+Revision ID: a1b2c3d4e5f6
+Revises: 1a2b3c4d5e6f
 Create Date: 2026-02-02 18:00:00.000000
 
 """
@@ -13,8 +13,8 @@ import sqlalchemy as sa
 import gpustack.utils.sql_enum as sql_enum
 
 # revision identifiers, used by Alembic.
-revision: str = "2026_02_02_1800-a1b2c3d4e5f6_add_aws_to_clusterprovider"
-down_revision: Union[str, None] = "53667f33f000"
+revision: str = "a1b2c3d4e5f6"
+down_revision: Union[str, None] = "1a2b3c4d5e6f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -36,6 +36,13 @@ table_columns = {
     "clusters": "provider",
 }
 
+downgrade_cluster_provider_enum = sa.Enum(
+    "Docker",
+    "Kubernetes",
+    "DigitalOcean",
+    "AWS",
+    name="clusterprovider",
+)
 
 def upgrade() -> None:
     """Add AWS to clusterprovider enum."""
@@ -52,10 +59,7 @@ def downgrade() -> None:
     # This would require creating a new enum, converting columns, and dropping the old one
     # For now, we skip the downgrade or document that it's not reversible
     sql_enum.remove_enum_values(
-        {
-            "cloud_credentials": ("provider", "DigitalOcean"),
-            "clusters": ("provider", "Docker"),
-        },
-        cluster_provider_enum,
+        table_columns,
+        downgrade_cluster_provider_enum,
         to_add_value,
     )
